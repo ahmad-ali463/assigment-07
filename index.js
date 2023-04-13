f7a1bdb490b85a5c2087fec7a08e98afccf38587
Ahmad Ali Asif <spyali463@gmail.com>
2023-04-13 21:17:56 +0500

    one to two programme

---
+++ index.js
@@ -0,0 +1,41 @@
+// // 1. Write a js program to find maximum between two numbers.
+// {
+//     var firstValue =+ prompt("Enter first value");
+//     var secondValue =+ prompt("Enter second value");
+//     if (firstValue > secondValue){
+//    console.log(`your Number1 "${firstValue}" is greater then Number2 "${secondValue}"`)
+// }
+//     else  if(firstValue<secondValue){
+//         console.log(`your Number1 ${firstValue
+//         } is Less to ${secondValue}`);
+//     }
+//     else  if(firstValue==secondValue){
+//         console.log(`your Number1 ${firstValue
+//         } is Equal then ${secondValue}`);
+//     }else{
+//         console.log('Please Enter a Vaild Value')
+//     }
+    
+// }
+// 2. Write a js program to find maximum between three numbers.
+//
+
+// function maximum(numb1 ,numb2 , numb3) {
+//     if (numb1 > numb2 && numb1 > numb3) {
+//         console.log(`Your Number1 "${numb1} "is Greater then NUmber2" ${numb2}" and Number3 
+//         "${numb3}"`);
+//     }
+//     else if (numb2 > numb3 && numb2 > numb1) {
+//         console.log(`${numb2} is Greater then ${numb1} and ${numb3}`);
+//     }
+//     else{
+//         console.log(`${numb3} is Greater then ${numb1} and ${numb2}`);
+//     }
+// }
+// numb1 = prompt("Enter first value");
+// numb2 = prompt("Enter second value");
+// numb3 = prompt("Enter third value");
+
+// maximum(numb1,numb2,numb3)
+
+// -----------------------------------------------------------------------------------------
